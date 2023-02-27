0676a3adbf0d9ef007bf13ea9ae8c7846e823099
chiragbarka <chirag.barka61@gmail.com>
2023-02-27 18:41:07 +0530

    api key issue resolved

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-const API_KEY = "AIzaSyAyjF12y5Xar-0a5WrpNeWonZHgWuj01oY";
+// const API_KEY = "AIzaSyAyjF12y5Xar-0a5WrpNeWonZHgWuj01oY";
+const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const YOUTUBE_API_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
